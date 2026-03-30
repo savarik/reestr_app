89c07040153e8d35f0a59f60c0f045d3fb2b9bd2
savarik <ishkov.savely@gmail.com>
2026-03-30 17:32:59 +0300

    создал DTO и омÐсоздал общий DTO и маппер

---
+++ src/main/java/com/pd/reestr/DTO/EquipmentDto.java
@@ -0,0 +1,26 @@
+package com.pd.reestr.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EquipmentDto {
+    private Long id;
+    private String name;
+    private String okid2;
+    private String measure;
+    private BigDecimal price;
+    private int count;
+    private int sum;
+    private BigDecimal debet;
+    private BigDecimal credit;
+    private String country;
+    private String description;
+    private String source_exel;
+}
